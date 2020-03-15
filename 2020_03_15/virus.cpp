d1d42a23dc37ae9e8acbbb85126c609b3351b88d
SangYeongLee <dltkddud4403@naver.com>
2020-03-15 17:33:30 +0900

    20200315 +2

--- 2020_03_15/virus.cpp
+++ 2020_03_15/virus.cpp
@@ -9,10 +9,11 @@ int main(){
 	int N,M;
 	int answer=0;
 	vector<vector<int>> board;
-	vector<vector<int>> blank;
-	vector<vector<int>> virus;
+	vector<vector<int>> blank;	//빈 칸 좌표
+	vector<vector<int>> virus;	//바이러스 좌표
 	cin>>N>>M;
 
+	//입력 받아서 위에 3가지 벡터 채움
 	for(int i=0;i<N+2;i++){
 		vector<int> temp;
 		for(int j=0;j<M+2;j++){
@@ -29,15 +30,19 @@ int main(){
 		board.push_back(temp);
 	}
 
+	//벽을 세울 위치 3개를 조합으로 구하기 위한 index 벡터
 	vector<int> ind={1,1,1};
 	for(int i=0;i<blank.size()-3;i++){
 		ind.push_back(0);
 	}
-
 	sort(ind.begin(),ind.end());
+
+	//dfs 돌때 인접한 칸 4개 비교를 편하게 하기위한 벡터
 	vector<vector<int>> move={{1,0},{-1,0},{0,1},{0,-1}};
 
+	//3개의 벽을 세울 수 있는 모든 조합에 대해 dfs
 	do{
+		//새로 세운 벽(3)
 		int cnt=0;
 		vector<vector<int>> temp = board;
 		for(int i=0;i<ind.size();i++){
@@ -45,25 +50,27 @@ int main(){
 				temp[blank[i][0]][blank[i][1]]=3;
 		}
 
+		//virus 벡터에 있는 모든 좌표 스택에 push
+		vector<vector<int>> stack;
 		for(int i=0;i<virus.size();i++){
-			vector<vector<int>> stack;
 			stack.push_back(virus[i]);
+		}
 
-			while(stack.size()!=0){
-				vector<int> cur = stack.back();
-				stack.pop_back();
-				
-				for(int j=0;j<move.size();j++){
-					if(temp[cur[0]+move[j][0]][cur[1]+move[j][1]]==0){
-						stack.push_back({cur[0]+move[j][0],cur[1]+move[j][1]});
-						temp[cur[0]+move[j][0]][cur[1]+move[j][1]]=2;
-						cnt++;
-					}
+		//dfs돌면서 퍼진 바이러스 개수 count
+		while(stack.size()!=0){
+			vector<int> cur = stack.back();
+			stack.pop_back();
+			
+			for(int j=0;j<move.size();j++){
+				if(temp[cur[0]+move[j][0]][cur[1]+move[j][1]]==0){
+					stack.push_back({cur[0]+move[j][0],cur[1]+move[j][1]});
+					temp[cur[0]+move[j][0]][cur[1]+move[j][1]]=2;
+					cnt++;
 				}
-
 			}
 		}
 
+		//빈 칸 개수에서 새로 세운 벽 3개랑 퍼진 바이러스 개수 빼서 최대값 계산
 		if(blank.size()-3-cnt > answer) 
 			answer = blank.size()-3-cnt;
 	}while(next_permutation(ind.begin(),ind.end()));
